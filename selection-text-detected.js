9b0f778b3eb07f464c7182eb29b18204aa724cbb
Thulio Bittencourt <bittencourtthulio@gmail.com>
2023-10-31 14:26:01 -0300

    Update selection-text-detected.js

--- selection-text-detected.js
+++ selection-text-detected.js
@@ -1,24 +1,13 @@
-document.addEventListener('mouseup', function(event) {
-  var selectedText = window.getSelection().toString();
-  var menu = document.getElementById('menu-suspenso');
-  console.log('chegou aqui');
-  if (selectedText !== '') {
-    console.log(selectedText);
-    // Define a posição do menu suspenso com base na posição do cursor do mouse
-    menu.style.left = (event.clientX + window.scrollX) + 'px';
-    menu.style.top = (event.clientY + window.scrollY) + 'px';
+document.onmouseup = document.onkeyup = document.onselectionchange = function() {
+    console.log(getSelectionText());
+};
 
-    // Injete o HTML do menu suspenso
-    menu.innerHTML = '<ul>' +
-                    '<li><a href="#">Opção 1</a></li>' +
-                    '<li><a href="#">Opção 2</a></li>' +
-                    '<li><a href="#">Opção 3</a></li>' +
-                    '</ul>';
-
-    // Exiba o menu suspenso
-    menu.style.display = 'block';
-  } else {
-    // Oculte o menu suspenso se nenhum texto estiver selecionado
-    menu.style.display = 'none';
-  }
-});
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
